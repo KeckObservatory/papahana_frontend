fca6922bf87d04b2f0e5cf75e51b4821745afca2
Lucas Fuhrman <lfuhrman@mac-lfuhrman.hawaii.rr.com>
2021-04-26 11:15:00 -1000

    initial addition of put, post, delete to api functions

--- src/api/ApiCore.tsx
+++ src/api/ApiCore.tsx
@@ -19,45 +19,29 @@ export class ApiCore {
 
     constructor(options: options) {
 
-        this.get = () => {
-            return api_funcs.get(options.url, options.api);
-        };
+        if (options.get) {
+            this.get = () => {
+                return api_funcs.get(options.url, options.api);
+            };
+        }
 
-        this.post = (model: object) => {
-            return api_funcs.post(options.url, options.api, model);
-        };
+        if (options.post) {
+            this.post = (model: object) => {
+                return api_funcs.post(options.url, options.api, model);
+            };
+        }
 
-        this.put = (model: object) => {
-            return api_funcs.put(options.url, options.api, model);
-        };
+        if (options.put) {
+            this.put = (model: object) => {
+                return api_funcs.put(options.url, options.api, model);
+            };
+        }
 
-        this.remove = (id: string) => {
-            return api_funcs.remove(options.url, options.api, id);
-        };
-
-        // if (options.get) {
-        //     this.get = () => {
-        //         return api_funcs.get(options.url, options.api);
-        //     };
-        // }
-        //
-        // if (options.post) {
-        //     this.post = (model: object) => {
-        //         return api_funcs.post(options.url, options.api, model);
-        //     };
-        // }
-        //
-        // if (options.put) {
-        //     this.put = (model: object) => {
-        //         return api_funcs.put(options.url, options.api, model);
-        //     };
-        // }
-        //
-        // if (options.remove) {
-        //     this.remove = (id: string) => {
-        //         return api_funcs.remove(options.url, options.api, id);
-        //     };
-        // }
+        if (options.remove) {
+            this.remove = (id: string) => {
+                return api_funcs.remove(options.url, options.api, id);
+            };
+        }
 
     }
 }
