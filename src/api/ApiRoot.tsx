47e8a2404d8e2d1af8a5a45b1e9f9216b5c0264f
tylertucker202 <tylertucker202@gmail.com>
2021-05-13 16:43:16 -1000

    using env variables to set backend url

--- src/api/ApiRoot.tsx
+++ src/api/ApiRoot.tsx
@@ -4,17 +4,23 @@
 import axios, { AxiosResponse } from 'axios';
 import { handleResponse, handleError } from './response';
 import { Document } from './../typings/papahana'
+import { pseudoRandomBytes } from 'crypto';
+
 
 // Define your api url from any source.
 // Pulling from your .env file when on the server or from localhost when locally
 
-const BASE_URL: any = {
-    papahana_demo: 'https://vm-appserver.keck.hawaii.edu/api/ddoi/v0/obsBlocks?',
-    papahana_local: 'http://localhost:50000/v0/obsBlocks?'
-}
+// const BASE_URL: any = {
+//     papahana_demo: 'https://vm-appserver.keck.hawaii.edu/api/ddoi/v0/obsBlocks?',
+//     papahana_local: 'http://localhost:50000/v0/obsBlocks?'
+// }
 
+var BASE_URL = 'http://localhost:50000/v0/obsBlocks?'
+console.log(BASE_URL)
+console.log(process.env)
 const get = (resource: string, api_type: string): Promise<Document> => {
-    const url = `${BASE_URL[api_type]}${resource}`
+    const url = `${BASE_URL}${resource}`
+    console.log(url)
     return axios
         .get(url)
         .then(handleResponse)
@@ -23,13 +29,13 @@ const get = (resource: string, api_type: string): Promise<Document> => {
 
 const post = (resource: string, api_type: string, model: object): Promise<any> => {
     return axios
-        .post(`${BASE_URL[api_type]}${resource}`, model)
+        .post(`${BASE_URL}${resource}`, model)
         .then(handleResponse)
         .catch(handleError);
 };
 
 const put = (resource: string, api_type: string, model: object): Promise<any> => {
-    const url = `${BASE_URL[api_type]}${resource}`
+    const url = `${BASE_URL}${resource}`
     console.log(`put url ${url}`)
     console.log(`model ${model}`)
     console.log(model)
@@ -41,7 +47,7 @@ const put = (resource: string, api_type: string, model: object): Promise<any> =>
 
 const remove = (resource: string, api_type: string ): Promise<any> => {
     return axios
-        .delete(`${BASE_URL[api_type]}${resource}`)
+        .delete(`${BASE_URL}${resource}`)
         .then(handleResponse)
         .catch(handleError);
 };
