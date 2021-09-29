dfd41d6f35cbae34998612af05a55ff742295502
tylertucker202 <tylertucker202@gmail.com>
2021-09-29 13:46:11 -1000

    remove svg children before rendering

--- src/components/PlanningTool/sky_view.tsx
+++ src/components/PlanningTool/sky_view.tsx
@@ -2,28 +2,40 @@ import { useD3 } from '../../hooks/useD3'
 import * as d3 from 'd3'
 import { OBCell, Target } from '../../typings/papahana'
 
+const HT_OFFSET = 600 // hawaii time offset from ut time
+const KECK_LONG = 360-155.4747 // absolute longitude of keck observatory
+const KECK_LAT = 19.8260
+
 const skyview = (svg: any, height: number, width: number, targets: Target[]) => {
     console.log('Targets in skyview:')
     console.log(targets)
     
-    let ra: number[] = targets.map((target: Target) => {
-        return target.ra_deg as number
-    })
-    let dec: number[] = targets.map((target: Target) => {
-        return target.dec_deg as number
+    let [ra, dec, alt, az]: number[][] = [[], [], [], []]
+
+    targets.forEach((target: Target) => {
+        ra.push(target.ra_deg as number)
+        dec.push(target.dec_deg as number)
+        const azAlt = ra_dec_to_az_alt(target.ra_deg as number, target.dec_deg as number)
+        az.push(azAlt[0])
+        alt.push(azAlt[1])
     })
 
     if (ra.length <= 0) return
+    const data = d3.zip(az, alt)
+    console.log( 'az, alt' )
+    console.log( az, alt )
+
+    d3.selectAll("svg > *").remove(); // clear old scales and points
 
     const x = d3.scaleLinear()
-        .domain([Math.min(...ra)-10, Math.max(...ra)+10])
+        .domain([Math.min(...az)-10, Math.max(...az)+10])
         .range([0, width]);
     svg.append("g")
         .attr("transform", `translate(0, ${height})`)
         .call(d3.axisBottom(x));
 
     const y = d3.scaleLinear()
-        .domain([Math.min(...dec)-10, Math.max(...dec)+10])
+        .domain([Math.min(...alt)-10, Math.max(...alt)+10])
         .range([height, 0]);
     svg.append("g")
         .call(d3.axisLeft(y));
@@ -41,7 +53,7 @@ const skyview = (svg: any, height: number, width: number, targets: Target[]) => 
         .style('pointer-events', 'none')
         .style("color", "white")
 
-    const txt = (d: any) => `target is ${d[0]} ra and ${d[0]} dec`
+const txt = (d: any) => `target is ${d[0]} azimuth and ${d[1]} altitude`
 
     // Three function that change the tooltip when user hover / move / leave a cell
     const mouseover = function (event: any, d: any) {
@@ -76,7 +88,7 @@ const skyview = (svg: any, height: number, width: number, targets: Target[]) => 
     svg
         .append("g")
         .selectAll("dot")
-        .data(d3.zip(ra, dec))
+        .data(data)
         .join("circle")
         .attr("class", "myCircle")
         .attr("cx", (d: any) => x(d[0]))
@@ -96,6 +108,23 @@ interface Props {
     selObs: OBCell[]
 }
 
+const date_to_juld = (date: Date) => {
+    return Math.floor( date.getTime()/864000 ) - (HT_OFFSET / 1440 ) + 2440587.5
+}
+
+const get_gmt = (date?: Date) => {
+    /* Taken from Jean Meeus's Astronomical Algorithms textbook. Using equations
+    12.1 & 12.4*/
+    if (!date) date = new Date()
+    const JD = date_to_juld(date)
+    const T = ( JD - 2_451_545.0 ) / 36_525 // 12.1
+    const Theta0 = 280.46061837 
+                   + 360.98564736629 * (JD - 2451545.0) 
+                   + T * T * 0.000387933 
+                   - T * T * T / 38710000 // 12.4
+    return Theta0
+}
+
 const hours_to_deg = (time: string, dec = false) => {
     let [hours, min, sec] = time.split(':')
     // let sign: number = dec ? (hours[0] === '+' ? 1 : -1) : 1
@@ -110,6 +139,20 @@ const hours_to_deg = (time: string, dec = false) => {
     return deg
 }
 
+const ra_dec_to_az_alt = (ra: number, dec: number, date?: Date) => {
+
+    if (!date) date = new Date()
+    const hourAngle = (get_gmt(date) - KECK_LONG) % 360
+    const sinAlt =  Math.sin(Math.PI / 180 * dec) * Math.sin(Math.PI / 180 * KECK_LAT) 
+            + Math.cos(Math.PI / 180 * dec) * Math.cos(Math.PI / 180 * KECK_LAT) * Math.cos(Math.PI / 180 * hourAngle)
+    const alt = Math.asin(sinAlt)
+    const A = ( Math.sin(Math.PI / 180 * dec) - Math.sin(alt) * Math.sin(Math.PI / 180 * KECK_LAT))
+              / Math.cos(alt) * Math.cos(Math.PI / 180 * KECK_LAT)
+    const az = Math.sin(hourAngle) < 0? A: 2 * Math.PI - A
+
+    return [ 180 / Math.PI * alt, 180 / Math.PI * az ]
+}
+
 export default function SkyView(props: Props) {
 
     let targets: Target[] = []
@@ -122,6 +165,8 @@ export default function SkyView(props: Props) {
         target.dec_deg = hours_to_deg(target.dec, true)
         return target
     })
+
+
 
     const ref = useD3((svg: any) => { skyview(svg, props.height, props.width, targets) })
 
@@ -140,6 +185,6 @@ export default function SkyView(props: Props) {
 }
 
 SkyView.defaultProps = {
-    width: 500,
+    width: 450,
     height: 500,
 }
