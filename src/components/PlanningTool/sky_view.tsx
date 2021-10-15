f858f4c3f1bb417de4346b37f978b0f32fe60818
tylertucker202 <tylertucker202@gmail.com>
2021-10-14 20:44:37 -1000

    tooltip shows y values

--- src/components/PlanningTool/sky_view.tsx
+++ src/components/PlanningTool/sky_view.tsx
@@ -5,12 +5,12 @@ import * as d3 from 'd3'
 import { OBCell, Target } from '../../typings/papahana'
 import { ra_dec_to_az_alt, get_nadir, hours_to_deg, get_target_traj, get_times } from './sky_view_util'
 
-interface Data {time: Date, value: number, type: string, tgt?: string, units?: string}
+interface Data { time: Date, value: number, type: string, tgt?: string, units?: string }
 
-const format_traj = ( trace: [number, number][], times: Date[], tgt: string, units?: string): Data[] => {
+const format_traj = (trace: [number, number][], times: Date[], tgt: string, units?: string): Data[] => {
     let data: Data[] = []
-    for (let idx=0; idx<times.length; idx++) {
-        const d: Data = {time: times[idx], value: trace[idx][1], units: units, type: 'trajectory', tgt: tgt}
+    for (let idx = 0; idx < times.length; idx++) {
+        const d: Data = { time: times[idx], value: trace[idx][1], units: units, type: 'trajectory', tgt: tgt }
         data.push(d)
     }
     return data
@@ -22,38 +22,38 @@ const skyview = (svg: any, height: number, width: number, targets: Target[]) => 
 
     const nadir = get_nadir()
 
-    // set the ranges
-    var x = d3.scaleTime().range([width, 0]);
-    var y = d3.scaleLinear().range([0, height]);
-    var valueLines: any = []
-    let myData: Data[][]= []
-    let mergedData: Data[]= []
+    let myData: Data[][] = []
+    let mergedData: Data[] = []
 
     targets.forEach((target: Target) => {
         ra.push(target.ra_deg as number)
         dec.push(target.dec_deg as number)
-        const times = get_times(nadir, 5)
+        const times = get_times(nadir, 25)
         const azAlt = ra_dec_to_az_alt(target.ra_deg as number, target.dec_deg as number, nadir)
         az.push(azAlt[0])
         alt.push(azAlt[1])
         const traj = get_target_traj(target.ra_deg as number, target.dec_deg as number, times)
-        const fTraj = format_traj(traj, times, target.name, 'degrees') 
-
-
-        // define the line
-        let valueLine = d3.line()
-            .x(function(d: any) { return x(d.time); })
-            .y(function(d: any) { return y(d.value); });
-
+        const fTraj = format_traj(traj, times, target.name, 'degrees')
         mergedData = [...mergedData, ...fTraj]
         myData.push(fTraj)
-        valueLines.push(valueLine)
     })
 
     if (ra.length <= 0) return
+    const startDate = myData[0][0].time
+    const endDate = myData[0][myData[0].length - 1].time
+    const values = myData.flat().map(d => d.value)
+    const yMin: number = d3.min(values) as number
+    const yMax: number = d3.max(values) as number
+
+    const xScale = d3.scaleTime([startDate, endDate],
+        [0, width]
+    )
+    const yScale = d3.scaleLinear([yMin, yMax],
+        [height, 0]
+    )
 
     let tdx = 0
-    const tgts = myData.map( d => {
+    const tgts = myData.map(d => {
         let t = d[0].tgt
         t = t ? t : JSON.stringify(tdx)
         tdx++
@@ -61,49 +61,22 @@ const skyview = (svg: any, height: number, width: number, targets: Target[]) => 
     })
 
     const colors = d3.scaleOrdinal(
-            tgts, d3.schemeCategory10
-        )
-
-
-    const startDate = myData[0][0].time
-    const endDate = myData[0][myData[0].length - 1].time
-    const values = myData.flat().map(d => d.value)
-    const yMin: number = d3.min( values ) as number
-    const yMax: number = d3.max( values ) as number
-
-    const xScale = d3.scaleTime( [startDate, endDate],
-            [0, width]
-            )
-    
-
-    const yScale = d3.scaleLinear( [yMin, yMax],
-            [height, 0]
-            )
-
-    const yAxis = () => {
-        return d3.axisLeft(yScale)
-    }
-
-    const xAxis = () => {
-        return d3.axisBottom(xScale)
-    }
+        tgts, d3.schemeCategory10
+    )
 
     const line = d3.line()
-    .x( (d: any | Data) => xScale(d.time) )
-    .y( (d: any | Data) => yScale(d.value) )
+        .x((d: any | Data) => xScale(d.time))
+        .y((d: any | Data) => yScale(d.value))
 
-    const dot = svg.append("g")
-    .attr("display", "none");
-
-    dot.append("circle")
-        .attr("r", 9.5);
-
-    dot.append("text")
-        .attr("font-family", "sans-serif")
-        .attr("font-size", 10)
-        .attr("text-anchor", "middle")
-        .attr("y", height-8);
-
+    for (const i in myData) {
+        svg.append('circle')
+            .attr('class', 'marker ' + myData[i][0].tgt)
+            .attr('cx', 100)
+            .attr('cy', 100)
+            .attr('r', 5)
+            .attr('fill', 'grey')
+            .style('opacity', 0)
+    }
 
     const tooltip = d3.select("body")
         .append("h1")
@@ -119,263 +92,113 @@ const skyview = (svg: any, height: number, width: number, targets: Target[]) => 
         .style("color", "white")
 
     const txt = (d: Data) => `target is ${d.tgt} with ${d.type} ${d.value} ${d.units}`
-
 
     const ruler = svg.append('rect')
-    .attr('x', 0)
-    .attr('y', 0)
-    .attr('height', height)
-    .attr('width', 2)
-    .attr('class', 'ruler')
-    .style( 'stroke', 'lightgray' )
-    .attr('opacity', 0)
+        .attr('x', 0)
+        .attr('y', 0)
+        .attr('height', height)
+        .attr('width', 2)
+        .attr('class', 'ruler')
+        .style('stroke', 'lightgray')
+        .attr('opacity', 0)
 
+    var bisectDate = d3.bisector(function(d: any) { return d.time; }).left;
+    const formatDate = (date: Date ) =>{
+        var year = date.getFullYear(),
+            month = date.getMonth() + 1, // months are zero indexed
+            day = date.getDate(),
+            hour = date.getHours(),
+            minute = date.getMinutes(),
+            second = date.getSeconds(),
+            hourFormatted = hour % 12 || 12, // hour returned in 24 hour format
+            minuteFormatted = minute < 10 ? "0" + minute : minute,
+            morning = hour < 12 ? "am" : "pm";
+    
+        return month + "/" + day + "/" + year + " " + hourFormatted + ":" +
+                minuteFormatted + morning;
+    }
 
     const moveRuler = (event: any) => {
-        const [ xp, yp ] = d3.pointer(event, svg.node())
-        const xpoint = x.invert(xp)
-        const ypoint = y.invert(yp)
-        console.log('pointer', xp, yp, ypoint)
-        ruler 
-            .attr('x', x(xpoint))
-            .style('opacity',1);
-        
+        const [xp, yp] = d3.pointer(event, svg.node())
+        const xpoint = xScale.invert(xp)
+
+        var d: any
+        var keys: any[] = []
+        for (const idx in myData) {
+            const i = bisectDate(myData[idx], xpoint, 1)
+            const d0 = myData[idx][i - 1]
+            const d1 = myData[idx][i]
+            d = xpoint.getTime() - d0.time.getTime() > d1.time.getTime() - xpoint.getTime() ? d1 : d0;
+            svg.selectAll('.marker.' + myData[idx][0].tgt)
+                .attr('cx', xScale(d.time))
+                .attr('cy', yScale(d.value))
+                .style('opacity', 1);
+
+            const c = colors(d.tgt)
+            const style:string = `{
+                width: 10px;
+                height: 10px;
+                margin-right: 8px;
+                border-radius: 2px;
+                background-color:${ c }
+                color:${ c }
+              }`
+                
+            const key =`<li style=${style}>
+                        <span  style=${ style }"></span>
+                        <span  style=${ style }>${ d.tgt }:</span>
+                        <span style=${ style }>${ d3.format( ',.3f' )( d.value ) }</span>
+                        </li>` 
+            keys.push(key)
+        }
+
+            tooltip
+                .style("opacity", 1)
+                .style("top", (event.pageY - 120) + "px")
+                .style("left", (event.pageX - 150) + "px")
+                .style("display", "inline-block")
+                .style("visibility", "visible")
+                .html(`
+                <div>
+                  <h4>${ formatDate(d.time)}</h4>
+                  <ul>
+                    ${ keys.join('') }
+                  </ul>
+                </div>
+              `)
+
+            ruler
+                .attr('x', xScale(d.time))
+                .style('opacity', 1);
+
+    }
+
+    const hideRuler = () => {
+        d3.selectAll('.ruler')
+            .style('opacity', 0);
+        d3.selectAll('.marker')
+            .style('opacity', 0);
         d3.selectAll('.label')
-            .attr('x', x(xpoint))
-            .attr('y', y(yp))
-            .text(xpoint.toString())
-            .style('opacity',1);
-        
+            .style('opacity', 0);
         tooltip
-            .style("opacity", 1)
-            .style("top", (event.pageY - 80) + "px")
-            .style("left", (event.pageX - 80) + "px")
-            .transition()
-            .style("visibility", "visible")
-            .text(xpoint.toISOString())
-
-        
-        // for (const i in series) {
-        //     const found_idx_points = series[i].data.map(d => d.x).indexOf(x_value)
-        //     if ( found_idx_points > -1) {
-        //     const y_value = series[i].data[found_idx_points].y
-        //     d3.selectAll('.marker.'+series[i].name)
-        //         .attr('cx', x(x_value))
-        //         .attr('cy', y(y_value))
-        //         .style('opacity',1);
-        //     } else {
-        //     d3.selectAll('.marker.'+series[i].name)
-        //         .style('opacity',0);
-        //     }
-        // }
-  }
-
-    const rect = svg.append('rect')
-    .attr('x', 0)
-    .attr('y', 0)
-    .attr('height', height)
-    .attr('width', width)
-    .attr('stroke', 'red')
-    .attr('stroke-width', '3px')
-    .attr('stroke-opacity', 0.3)
-    .attr('fill', 'none')
-
-    svg
-    .on("mousemove", moveRuler)
-    .on("mouseleave", hideRuler)
-
-
-    const lineClass = svg.selectAll('path')
-    .data( myData )
-    .join('path')
-      .attr('class', 'chart-lines')
-      // Using our line generator here
-      .attr('d', line)
-      // Every data point in the array has a name key
-      // so we just grab the one from d[0]
-      .style('stroke', (d: any | Data[]) => colors(d[0].tgt))
-      .style('stroke-width', 2)
-      .style('fill', 'transparent')
-}
-
-const hideRuler = () => {
-    d3.selectAll('.ruler')
-      .style('opacity',0);
-    d3.selectAll('.marker')
-      .style('opacity',0);
-      d3.selectAll('.label')
-        .style('opacity',0);
-  }
-
-
-const skyviewBak = (svg: any, height: number, width: number, targets: Target[]) => {
-    // console.log('Targets in skyview:')
-    // console.log(targets)
-    
-    let [ra, dec, alt, az]: number[][] = [[], [], [], []]
-    let myData: Data[] = [] 
-
-    const nadir = get_nadir()
-
-    // set the ranges
-    var x = d3.scaleTime().range([width, 0]);
-    var y = d3.scaleLinear().range([0, height]);
-    var valueLines: any = []
-
-    targets.forEach((target: Target) => {
-        ra.push(target.ra_deg as number)
-        dec.push(target.dec_deg as number)
-        const times = get_times(nadir, 5)
-        const azAlt = ra_dec_to_az_alt(target.ra_deg as number, target.dec_deg as number, nadir)
-        az.push(azAlt[0])
-        alt.push(azAlt[1])
-        const traj = get_target_traj(target.ra_deg as number, target.dec_deg as number, times)
-        const fTraj = format_traj(traj, times, target.name, 'degrees') 
-
-
-        // define the line
-        let valueLine = d3.line()
-            .x(function(d: any) { return x(d.time); })
-            .y(function(d: any) { return y(d.value); });
-
-        myData = [...myData, ...fTraj]
-        valueLines.push(valueLine)
-
-
-    })
-
-    console.log('myData', myData)
-    if (ra.length <= 0) return
-
-    const minTime = myData.reduce(function (a: Data, b: Data) { return a.time < b.time ? a: b; }); 
-    const maxTime = myData.reduce(function (a: Data, b: Data) { return a.time > b.time ? a : b; }); 
-    const minValue = myData.reduce(function (a: Data, b: Data) { return a.value < b.value ? a: b; });
-    const maxValue = myData.reduce(function (a: Data, b: Data) { return a.value > b.value ? a: b; });
-
-    x.domain([minTime.time, maxTime.time])
-    y.domain([minValue.value, maxValue.value])
-
-    // const data = d3.zip(az, alt)
-    // console.log( 'az, alt' )
-    // console.log( az, alt )
-
-    // d3.selectAll("svg > *").remove(); // clear old scales and points
-
-    // const x = d3.scaleLinear()
-    //     .domain([Math.min(...az)-10, Math.max(...az)+10])
-    //     .range([width, 0]);
-    // svg.append("g")
-    //     .attr("transform", `translate(0, ${height})`)
-    //     .call(d3.axisBottom(x));
-
-    // const y = d3.scaleLinear()
-    //     .domain([Math.min(...alt)-10, Math.max(...alt)+10])
-    //     .range([0, height]);
-    // svg.append("g")
-    //     .call(d3.axisLeft(y));
-
-    const tooltip = d3.select("body")
-        .append("h1")
-        .style("position", "absolute")
-        .style("background-color", "#515151")
-        .style("border-width", "1px")
-        .style("border-radius", "5px")
-        .style("opacity", .85)
-        .style("font", "13px sans-serif")
-        .style("padding", "10px")
-        .style("visibility", "hidden")
-        .style('pointer-events', 'none')
-        .style("color", "white")
-
-    const txt = (d: Data) => `target is ${d.tgt} with ${d.type} ${d.value} ${d.units}`
-
-    // Three function that change the tooltip when user hover / move / leave a cell
-    const mouseover = function (event: any, d: Data) {
-        tooltip
-            .style("opacity", 1)
-            .style("top", (event.pageY - 80) + "px")
-            .style("left", (event.pageX - 80) + "px")
-            .transition()
-            .style("visibility", "visible")
-            .text(txt(d))
-    }
-
-    const mousemove = function (event: any, d: Data) {
-        tooltip
-            .style("opacity", 1)
-            .style("top", (event.pageY - 80) + "px")
-            .style("left", (event.pageX - 80) + "px")
-            .transition()
-            .delay(300)
-            .duration(400)
-            .style("visibility", "visible")
-            .text(txt(d))
-    }
-
-    const mouseleave = function (event: any, d: Data) {
-        tooltip
-            .transition()
-            .delay(300)
-            .duration(400)
             .style("opacity", 0)
     }
 
-//   // Add the valueline path.
-//     valueLines.forEach( (line: any) => {
-//         svg.append("path")
-//             // .selectAll(".targets")
-//             .datum(myData)
-//             .attr("fill", "none")
-//             .attr("stroke", "steelblue")
-//             .attr("stroke-width", 1.5)
-//             .attr("stroke-linejoin", "round")
-//             .attr("stroke-linecap", "round")
-//             .attr("d", line)
-//             .on("mouseover", mouseover)
-//             .on("mousemove", mousemove)
-//             .on("mouseleave", mouseleave)
-//     })
+    svg
+        .on("mousemove", moveRuler)
+        .on("mouseleave", hideRuler)
 
-    var tgts = svg.selectAll(".targets")
-    .data(myData).enter().append('g').attr("class", "target")
-
-    var lines = document.getElementsByClassName('line')
-
-    var line = d3.line()
-      .x(function(d: any) {
-        return x(d.time);
-      })
-      .y(function(d: any) {
-        return y(d.value);
-      });
-
-    tgts.append("path")
-    .attr("class", "line")
-    .attr("d", function(d: any) { 
-        return line(d)
-    })
-    .attr("stroke", "steelblue")
-    .attr("stroke-width", 1.5)
-    .attr("stroke-linejoin", "round")
-    .attr("stroke-linecap", "round")
-
-    var mouseG = svg.append("g")
-    .attr("class", "mouse-over-effects");
-
-    mouseG.append("path") // this is the black vertical line to follow mouse
-        .attr("class", "mouse-line")
-        .style("stroke", "black")
-        .style("stroke-width", "1px")
-        .style("opacity", "0");
-
-    // Add the X Axis
-    svg.append("g")
-        .attr("transform", "translate(0," + height + ")")
-        .call(d3.axisBottom(x));
-
-    // Add the Y Axis
-    svg.append("g")
-        .call(d3.axisLeft(y));
+    const lineClass = svg.selectAll('path')
+        .data(myData)
+        .join('path')
+        .attr('class', 'chart-lines')
+        // Using our line generator here
+            .attr('d', line.curve(d3.curveNatural))
+        // Every data point in the array has a name key
+        // so we just grab the one from d[0]
+        .style('stroke', (d: any | Data[]) => colors(d[0].tgt))
+        .style('stroke-width', 2)
+        .style('fill', 'transparent')
 
 }
 
