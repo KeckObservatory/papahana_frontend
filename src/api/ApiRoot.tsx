25e633ceb0895945a681ab28ec441314f4375aa7
tylertucker202 <tylertucker202@gmail.com>
2022-03-04 11:21:09 -1000

    https

--- src/api/ApiRoot.tsx
+++ src/api/ApiRoot.tsx
@@ -12,7 +12,7 @@ import { mock_get_instrument_package, mock_get_template, mock_get_containers, mo
 
 const IS_PRODUCTION: boolean = process.env.REACT_APP_ENVIRONMENT==='production'
 console.log(`is PRODUCTION? set to ${IS_PRODUCTION}`) 
-var PRODUCTION_URL = 'http://www3build.keck.hawaii.edu/api/ddoi/'
+var PRODUCTION_URL = 'https://www3build.keck.hawaii.edu/api/ddoi/'
 var DEV_URL = 'http://localhost:50000/v0/' //use locally or for testing (npm start or npm run demobuild)
 var BASE_URL = IS_PRODUCTION ? PRODUCTION_URL : DEV_URL // sets for production vs dev
 var OB_URL = BASE_URL + 'obsBlocks?' 
