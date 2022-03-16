5a3368ac80c235adb861778d952bcc0446efb52f
tylertucker202 <tylertucker202@gmail.com>
2022-03-15 16:28:15 -1000

    Add container table to ob selection

--- src/components/OBSelect/ob_select.tsx
+++ src/components/OBSelect/ob_select.tsx
@@ -7,6 +7,11 @@ import { Paper } from '@mui/material'
 import SemidTree from './semid_tree'
 import { useObserverContext } from './../App'
 import ContainerTree from './container_tree'
+import ContainerTable from './container_table'
+
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Box from '@mui/material/Box';
 
 export interface Props {
   handleOBSelect: Function
@@ -19,6 +24,7 @@ interface State {
   containerIdList: string[]
   container_id: string
   sem_id: string
+  tabNumber: number
 }
 
 
@@ -28,14 +34,42 @@ const defaultState: State = {
   containerIdList: [],
   sem_id: '',
   container_id: 'all',
+  tabNumber: 0
 }
 
 
 const SemIDContext = createContext<[string, Function]>( ['', ()=>{}] )
 export const useSemIDContext = () => useContext(SemIDContext)
 
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: number;
+  value: number;
+}
+
+function TabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          {children}
+        </Box>
+      )}
+    </div>
+  );
+}
+
 export default function ObservationBlockSelecter(props: Props) {
   const [obList, setOBList] = useState(defaultState.obList)
+  const [tabNumber, setTabNumber] = useState(defaultState.tabNumber)
   const [semIdList, setSemIdList] = useState(defaultState.semIdList)
   const [containerIdList, setContainerIdList] = useState(defaultState.containerIdList)
   const observer_id = useObserverContext()
@@ -78,6 +112,10 @@ export default function ObservationBlockSelecter(props: Props) {
       .then(() => { reset_container_and_ob_select() })
   }, [observer_id])
 
+  const handleTabChange = (event: React.SyntheticEvent, newValue: number) => {
+      setTabNumber(newValue)
+  }
+
 
   return (
     <SemIDContext.Provider value={[sem_id, reset_container_and_ob_select]}>
@@ -91,7 +129,16 @@ export default function ObservationBlockSelecter(props: Props) {
       />
       <Paper>
         {/* <SemidTree sem_id={sem_id} handleOBSelect={props.handleOBSelect} /> */}
+        <Tabs value={tabNumber} onChange={handleTabChange}>
+          <Tab label="Containers" />
+          <Tab label="OBs" />
+        </Tabs>
+        <TabPanel value={tabNumber} index={0}>
         <ContainerTree handleOBSelect={props.handleOBSelect}/>
+        </TabPanel>
+        <TabPanel value={tabNumber} index={1}>
+        <ContainerTable/>
+        </TabPanel>
       </Paper>
     </div>
     </ SemIDContext.Provider>
