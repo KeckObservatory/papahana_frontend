de7e24bc76175e5e7e8f022bfb9ac8584f30ea7e
tylertucker202 <tylertucker202@gmail.com>
2022-03-28 14:42:14 -1000

    update sequence

--- src/components/ODT/sequence_grid/ob_form_beautiful_dnd.tsx
+++ src/components/ODT/sequence_grid/ob_form_beautiful_dnd.tsx
@@ -208,7 +208,7 @@ const createForm = (id: string, obComponent: OBComponent, updateOB): JSX.Element
 const updateOBScience = (seqName: string, ob: ObservationBlock, formData: OBSequence): ObservationBlock => {
     let newOb = { ...ob }
     //get science idx from name
-    console.log('updating component', seqName, formData, component)
+    console.log('updating component', seqName, formData)
     const idx = JSON.parse(seqName.substring(seqName.indexOf('_') + 1))
     let seq = ob.observations as Science[]
     if (seq) {
