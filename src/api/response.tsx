3fae535e94e533031f6b920731d0942168a742d4
tylertucker202 <tylertucker202@gmail.com>
2022-04-06 15:57:46 -1000

    error handling with interceptor

--- src/api/response.tsx
+++ src/api/response.tsx
@@ -1,12 +1,10 @@
 import { toast } from 'react-toastify'
 import axios, {AxiosError, AxiosResponse } from 'axios';
-
 export function handleResponse(response: AxiosResponse) {
     if (response.status > 200) {
         console.log('response status >200', response)
         toast.error(response.statusText)
     }
-    console.log('response', response)
 
     if (response.data) {
         return response.data;
@@ -15,8 +13,8 @@ export function handleResponse(response: AxiosResponse) {
     return response;
 }
 
-export function handleError(error: AxiosError) {
-    toast.error(error.response?.data)
+export function handleError(error: Error | AxiosError) {
+    toast.error(error.message)
     if (axios.isAxiosError(error)) {
         console.error('handleError: ', error)
         console.error(error.toJSON())
