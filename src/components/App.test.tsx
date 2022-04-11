f71ca1086f0628e7f8b670e0448ef7c8e97fd1eb
tylertucker202 <tylertucker202@gmail.com>
2022-04-11 08:57:27 -1000

    test bed setup

--- src/components/App.test.tsx
+++ src/components/App.test.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { Router, Route } from 'react-router';
+import { createBrowserHistory } from 'history';
+import { QueryParamProvider } from 'use-query-params';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+const history = createBrowserHistory();
+
+describe("<App />", () => {
+  test('renders learn react link', async () => {
+    render(
+      <div>
+        <Router {...{ history }}>
+          <QueryParamProvider ReactRouterRoute={Route}>
+            <App />
+          </QueryParamProvider>
+        </Router>,
+      </div>
+    );
+  });
+})
