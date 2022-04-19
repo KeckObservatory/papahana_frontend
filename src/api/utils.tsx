3760c9cff09102659e13fcded194ebf2252ad3c0
tylertucker202 <tylertucker202@gmail.com>
2022-04-19 08:12:48 -1000

    no await in forEach

--- src/api/utils.tsx
+++ src/api/utils.tsx
@@ -87,7 +87,7 @@ export const make_all_ob_container = async (sem_id: string, detailedContainers: 
 
 export const make_detailed_containers = async (sem_id: string, containers: Container[]) => { // adds all obs in a special container
    const detailedContainers: DetailedContainer[] = [];
-   await containers.forEach(async (container: Container) => {
+   containers.forEach(async (container: Container) => {
       // let dContainer: Partial<DetailedContainer> = container
       let partialObs: Partial<ObservationBlock>[] = []
       if (sem_id) {
