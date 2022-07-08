bfe6be923a456a1d49d8094258b0025d84a26bbe
tylertucker202 <tylertucker202@gmail.com>
2022-07-08 11:02:46 -1000

    cannot async inside of a foreach

--- src/components/ODTGlobe/global_observing_definition_tool_view.tsx
+++ src/components/ODTGlobe/global_observing_definition_tool_view.tsx
@@ -25,13 +25,15 @@ const get_all_ob_rows = async (): Promise<GlobalTableRow[]> => {
 
     let rows: GlobalTableRow[] = []
     const sem_ids = await get_sem_id_list()
-    await sem_ids.associations.forEach(async (sem_id: string) => {
+    for (let idx = 0; idx < sem_ids.associations.length; idx++) {
+        const sem_id = sem_ids.associations[idx]
         const obs = await semid_api_funcs.get_semester_obs(sem_id)
         const obRows = obs_to_rows(obs)
         rows = [...rows, ...obRows]
-    })
+        console.log('ob rows length: ', rows.length)
+    }
 
-    console.log('all ob rows length: ', rows.length)
+    console.log('final ob rows length: ', rows.length)
     return (rows)
 }
 
@@ -52,7 +54,7 @@ const obs_to_rows = (obs: ObservationBlock[]) => {
             instrument: ob.metadata.instrument,
             tags: ob.metadata.tags as string[],
             target: ob.target?.metadata.name,
-            acquisition: ob.acquisition.metadata.ui_name,
+            acquisition: ob.acquisition?.metadata.ui_name,
             common_parameters: ob.common_parameters?.metadata.ui_name,
             observations: seqLabel
         }
