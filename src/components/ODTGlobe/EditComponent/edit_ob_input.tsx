a857755dec26dcf7e9536f12d2673a4bcb876746
tylertucker202 <tylertucker202@gmail.com>
2022-07-28 11:46:22 -1000

    display location

--- src/components/ODTGlobe/EditComponent/edit_ob_input.tsx
+++ src/components/ODTGlobe/EditComponent/edit_ob_input.tsx
@@ -10,7 +10,8 @@ const EditOBInput = (value: string, tableMeta: any, updateValue: any) => {
         const ob_id = tableMeta.rowData[0]
         const sem_id = tableMeta.rowData[3]
         console.log(window.location)
-        const url = `${window.location.origin}?drawerOpen=1&ob_id=${ob_id}&sem_id=${sem_id}&tab_index=0`
+        let url = `${window.location.origin}${window.location.pathname}`
+        url += `?drawerOpen=1&ob_id=${ob_id}&sem_id=${sem_id}&tab_index=0`
         console.log(url)
         const win = window.open(url, '_blank');
         win?.focus();
