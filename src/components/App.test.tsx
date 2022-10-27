174b10f8e6dccfa750fbf62d387075b2c2da7d86
tylertucker202 <tylertucker202@gmail.com>
2022-10-26 15:35:18 -1000

    testing mods

--- src/components/App.test.tsx
+++ src/components/App.test.tsx
@@ -3,24 +3,21 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 import { Router, Route } from 'react-router';
 import { createBrowserHistory } from 'history';
-import { shallow } from "enzyme";
 import { QueryParamProvider } from 'use-query-params';
 
 const history = createBrowserHistory();
 
 describe("<App />", () => {
-  it('renders learn react link', async () => {
-    shallow(
-      <div>
+  it('renders learn react link', () => {
+    render(
         <Router {...{ history }}>
           <QueryParamProvider ReactRouterRoute={Route}>
             <App />
           </QueryParamProvider>
         </Router>,
-      </div>
     );
   });
   
-  // const linkElement = screen.getByText(/Papahana/i);
-  // expect(linkElement).toBeInTheDocument();
+  const linkElement = screen.getByText(/Papahana/);
+  expect(linkElement).toBeInTheDocument();
 })
